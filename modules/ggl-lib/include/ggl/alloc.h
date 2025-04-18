4197c8098071b17c33c28e36237dc72771c8734f
Archit Gupta <archigup@amazon.com>
2025-04-18 13:12:29 -0700

    WIP: New alloc interface

--- modules/ggl-lib/include/ggl/alloc.h
+++ modules/ggl-lib/include/ggl/alloc.h
@@ -31,4 +31,34 @@ void ggl_free(GglAlloc *alloc, void *ptr);
 #define GGL_ALLOCN(alloc, type, n) \
     (typeof(type) *) ggl_alloc(alloc, (n) * sizeof(type), alignof(type))
 
+typedef struct {
+    void *val;
+} GglAllocCtx;
+
+/// Allocator vtable.
+typedef struct {
+    void *(*const ALLOC)(GglAllocCtx ctx, size_t size, size_t alignment);
+    void (*const FREE)(GglAllocCtx ctx, void *ptr);
+} GglAllocVtable;
+
+typedef struct {
+    const GglAllocVtable *const VTABLE;
+    const GglAllocCtx CTX;
+} GglAlloc2;
+
+/// Allocate memory from an allocator.
+/// Prefer `GGL_ALLOC` or `GGL_ALLOCN`.
+void *ggl_alloc2(GglAlloc2 *alloc, size_t size, size_t alignment);
+
+/// Free memory allocated from an allocator.
+/// Prefer `GGL_AUTOFREE` for scope-bound values.
+void ggl_free2(GglAlloc2 *alloc, void *ptr);
+
+/// Allocate a `type` from an allocator.
+#define GGL_ALLOC2(alloc, type) \
+    (typeof(type) *) ggl_alloc2(alloc, sizeof(type), alignof(type))
+/// Allocate `n` units of `type` from an allocator.
+#define GGL_ALLOCN2(alloc, type, n) \
+    (typeof(type) *) ggl_alloc2(alloc, (n) * sizeof(type), alignof(type))
+
 #endif
